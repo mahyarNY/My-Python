501824d77cb19b042efd174de79615a4e314b087
Mahyar <mahyarb.86@gmail.com>
2017-06-05 23:09:18 +0430

    Update remove_duplicates.py

--- remove_duplicates.py
+++ remove_duplicates.py
@@ -1,10 +1,16 @@
 def remove_duplicates(list_):
-    count = 1
+    turn = 0
+    index_list = []
     for item in list_:
-        while count <= len(list_):
+        count = 1 + turn
+        while count < len(list_):
             if item == list_[count]:
-                list_.remove(list_[count])
+                index_list.append(list_.index(list_[count]))
             count += 1
+        turn += 1
+    for e in index_list:
+        list_.remove(list_[e])
     return list_
 
 print (remove_duplicates([1, 2, 2, 1]))
+
