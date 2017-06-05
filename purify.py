b564dc0d9f6d5cc81d09ccd6564e5968d87d1e64
Mahyar <mahyarb.86@gmail.com>
2017-06-05 19:11:25 +0430

    Update purify.py

--- purify.py
+++ purify.py
@@ -1,9 +1,16 @@
-def purify(list_purify):
+def purify(list_):
     list_new = []
-    for i in list_purify:
+    index_list = []
+    no_list = []
+    for i in list_:
         if i % 2 != 0:
-            list_new = list_purify.remove(i)
+            index_list.append(list_.index(i))
+        
+    for e in index_list:
+        no_list.append(list_[e])
+    for item in list_:
+        if item not in no_list:
+            list_new.append(item)
     return list_new
-
 my_list = [1, 2, 4, 7]
 print purify(my_list)
