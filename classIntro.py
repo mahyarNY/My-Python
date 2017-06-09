0927a1dd5661eb6b9b4ca4beeefdefa778540cd9
Mahyar <mahyarb.86@gmail.com>
2017-06-09 14:59:08 +0430

    calling a class instance

--- classIntro.py
+++ classIntro.py
@@ -20,3 +20,18 @@ class Equilateral(Triangle):
         self.angle1 = self.angle
         self.angle2 = self.angle
         self.angle3 = self.angle
+class Car(object):
+    condition = "new"
+    def __init__(self, model, color, mpg):
+        self.model = model
+        self.color = color
+        self.mpg   = mpg
+    def display_car(self):
+        return "This is a %s %s with %s MPG." %(self.color, self.model, str(self.mpg))
+    def drive_car(self):
+        self.condition = "used"
+
+my_car = Car("DeLorean", "silver", 88)
+print my_car.condition
+my_car.drive_car()
+print my_car.condition
